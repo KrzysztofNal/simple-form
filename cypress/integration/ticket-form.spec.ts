bcfc5dbb44d92a0c94c26996840d99cf645b63e1
KrzysztofNal <krzysztof.nalewajek@wp.pl>
2022-07-14 16:47:21 +0200

    Test

--- cypress/integration/ticket-form.spec.ts
+++ cypress/integration/ticket-form.spec.ts
@@ -1,1 +1,33 @@
 // Your tests go in here. Happy coding! 🤓
+describe('1', () => {
+    //Positive
+    it('successfully loads', () => {
+        cy.visit('http://localhost:3000'),
+            cy.get('#name').type('Jan'),
+            cy.get('#email').type('jan@test.fi'),
+            cy.get('#subject').type('test'),
+            cy.get('#message').type('My message'),
+            cy.get('button').click()
+    })
+})
+
+context('Errors', () => {
+    const errorMsg = 'Internal server error'
+
+    it('simulates a server error', () => {
+        cy.intercept('GET', '**/search?query=cypress', { statusCode: 500 }).as(
+            'getServerFailure'
+        )
+
+        cy.visit('http://localhost:3000')
+
+        cy.get('#name').type('Jan'),
+            cy.get('#email').type('jan@test.fi'),
+            cy.get('#subject').type('test'),
+            cy.get('#message').type('My message'),
+            cy.get('button').click(),
+            cy.wait('@getServerFailure')
+
+        cy.contains(errorMsg).should('be.visible')
+    })
+})
